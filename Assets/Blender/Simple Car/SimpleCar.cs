5ecf7f99dc37b4cc9ef24a185137940aec17ced2
Rico Tiongson <jetplanetiongson@yahoo.com>
2014-12-08 22:33:00 +0800

    Divided roads. Added intersection. Added car prototype. Let there be light.

--- Assets/Blender/Simple Car/SimpleCar.cs
+++ Assets/Blender/Simple Car/SimpleCar.cs
@@ -2,17 +2,17 @@
 using System.Collections;
 
 [ExecuteInEditMode]
-public class RoadMaker : MonoBehaviour {
+public class SimpleCar : MonoBehaviour {
 
-
+	public Material material;
 
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		foreach (Transform child in transform)
+			child.renderer.material = material;
 	}
 }
