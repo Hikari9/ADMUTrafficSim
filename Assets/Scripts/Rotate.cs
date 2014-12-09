e7b393e20749c243bdfd6f9300a6fe78faf285d2
Rico Tiongson <jetplanetiongson@yahoo.com>
2014-12-09 13:48:39 +0800

    Added rotating MMDA in MainMenu.

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Rotate : MonoBehaviour {
+
+	public float Speed = 1f;
+	public float DegreesPerSecond = 60f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.Rotate (new Vector3(0, this.DegreesPerSecond * this.Speed * Time.deltaTime));
+	}
+}
