11a880938d572482a0d8f1a1cd6994614bc747a1
Hikari9 <thericotiongson@gmail.com>
2014-12-26 18:10:35 +0800

    Added Glow Scripts. Moved scripts to correct dir.

---
+++ Assets/Scripts/Glow.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Glow : MonoBehaviour {
+
+	Color targetColor, originalColor;
+	public float colorFadeSpeed = 7.5f;
+	public bool testRedGreen = false;
+
+	// Use this for initialization
+	void Start () {
+		originalColor = targetColor = renderer.sharedMaterial.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (testRedGreen) {
+			if (Input.GetKey ("g")) {
+					setColor (Color.green);
+			} else if (Input.GetKey ("r")) {
+					setColor (Color.red);
+			} else if (Input.GetKey (KeyCode.Space)) {
+					setColor (originalColor);
+			}
+		}
+
+		Color currentColor = renderer.sharedMaterial.color;
+		// Color deltaColor = new Color (targetColor.r - currentColor.r, targetColor.g - currentColor.g, targetColor.b - currentColor.b, targetColor.a - currentColor.a);            targetColor.a - currentColor.a);
+		Color deltaColor = targetColor - currentColor;
+		renderer.sharedMaterial.color += deltaColor * Time.smoothDeltaTime * colorFadeSpeed;
+	}
+
+	public void setColor(Color color) {
+		this.targetColor = color;
+	}
+}
