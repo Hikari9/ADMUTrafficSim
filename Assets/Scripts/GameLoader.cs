decebb06ec8d3db6134de27f33598204be38853c
Rico Tiongson <thericotiongson@gmail.com>
2014-12-28 15:43:09 +0800

    Exit button works now.

--- Assets/Scripts/GameLoader.cs
+++ Assets/Scripts/GameLoader.cs
@@ -3,18 +3,16 @@ using System.Collections;
 
 public class GameLoader : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void LoadLevel(string level) {
 		Debug.Log ("Loading level: " + level);
 		Application.LoadLevel (level);
 	}
+
+	public void Quit() {
+		Application.Quit ();
+	}
+
+	void Update() {
+	}
+
 }
