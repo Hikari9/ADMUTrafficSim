59a11e678732485b7cfa36edd3417fabb35da478
Rico Tiongson <thericotiongson@gmail.com>
2014-12-28 12:34:15 +0800

    Added build data. Functionalized New Game button.

---
+++ Assets/Scripts/GameLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLoader : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadLevel(string level) {
+		Debug.Log ("Loading level: " + level);
+		Application.LoadLevel (level);
+	}
+}
