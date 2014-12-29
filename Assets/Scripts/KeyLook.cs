247d87910d040772a8603135dc43bc6f830807f9
NINA <sanchez.jenina@gmail.com>
2014-12-29 17:39:58 +0800

    The message.

--- Assets/Scripts/KeyLook.cs
+++ Assets/Scripts/KeyLook.cs
@@ -1,7 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System;
 
 [ExecuteInEditMode]
 public class KeyLook : MonoBehaviour {
@@ -16,22 +15,19 @@ public class KeyLook : MonoBehaviour {
 	}
 
 	private float rotationAngle = 0;
-	
+
 	// Update is called once per frame
 	void Update () {
-		foreach (KeyCode key in leftLook) {
+		foreach (KeyCode key in leftLook)
 			if (Input.GetKeyDown (key)) {
 				rotationAngle -= 90;
 				break;
 			}
-		}
-		foreach (KeyCode key in rightLook){
+		foreach (KeyCode key in rightLook)
 			if (Input.GetKeyDown (key)) {
 				rotationAngle += 90;
 				break;
 			}
-		}
-		
 		float rot = Time.smoothDeltaTime * rotationAngle * rotationSpeed;
 		transform.Rotate (0, rot, 0);
 		rotationAngle -= rot;
