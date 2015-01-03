c3be06161595f37febc3ca2fd649e214c846674a
Rico Tiongson <thericotiongson@gmail.com>
2015-01-03 17:42:51 +0800

    Added pause game.

---
+++ Assets/GamePauser.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePauser : MonoBehaviour {
+
+	protected bool Pausing = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space))
+			Pausing ^= true;
+		Time.timeScale = Pausing ? 0f : 1f;
+	}
+}
--- Assets/Scripts/CarSpawner.cs
+++ Assets/Scripts/CarSpawner.cs
@@ -71,8 +71,8 @@ public class CarSpawner : MonoBehaviour {
 		car.tag = "car";
 
 		var tail = GetRoadTail (currentRoad);
-		if (tail)
-						Debug.Log (tail.transform.parent.InverseTransformPoint (tail.renderer.bounds.center + tail.renderer.bounds.extents).z);
+		// if (tail)
+			// Debug.Log (tail.transform.parent.InverseTransformPoint (tail.renderer.bounds.center + tail.renderer.bounds.extents).z);
 		car.transform.localPosition += new Vector3 (DEFAULT_NORTH_POSITION.x, 0, tail == null ? DEFAULT_NORTH_POSITION.y : Mathf.Max (DEFAULT_NORTH_POSITION.y, tail.transform.parent.InverseTransformPoint (tail.renderer.bounds.center + tail.renderer.bounds.extents).z + car.transform.parent.InverseTransformPoint(car.renderer.bounds.extents).z + 5f));
 
 		// car.transform.localRotation = new Quaternion (-0.7f, 0, 0.7f, 0);
