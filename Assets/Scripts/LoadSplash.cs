e033d3648631b40040dc3e5324027fba0731f035
NINA <sanchez.jenina@gmail.com>
2015-01-03 15:49:19 +0800

    Added splash screen.

---
+++ Assets/Scripts/LoadSplash.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadSplash : MonoBehaviour {
+
+	public Texture splash;
+	// public int width;
+	// public int height;
+	private Rect r;
+	
+	// Use this for initialization
+	void Start () {
+		r = new Rect(0, 0, Screen.width, Screen.height);
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		GUI.DrawTexture(r, splash);
+		
+		if(Input.anyKey) {
+			Application.LoadLevel("Main");
+		}
+	}
+}
