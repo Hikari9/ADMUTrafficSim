775688355c2d2a07817f52585d1fa459ed144242
Rico Tiongson <thericotiongson@gmail.com>
2015-01-03 18:06:24 +0800

    Added pause game GUI.

--- Assets/GamePauser.cs
+++ Assets/GamePauser.cs
@@ -16,4 +16,15 @@ public class GamePauser : MonoBehaviour {
 			Pausing ^= true;
 		Time.timeScale = Pausing ? 0f : 1f;
 	}
+
+	void OnGUI() {
+		if (Pausing) {
+			GUIStyle style = new GUIStyle(GUI.skin.GetStyle ("label"));
+
+			style.fontSize = 100;
+			style.hover.textColor = style.normal.textColor = Color.white;
+
+			GUI.Box (new Rect ((Screen.width)/2 -(Screen.width)/4,(Screen.height)/2-(Screen.height)/4,(Screen.width)/2,(Screen.height)/2), "GAME PAUSED", style);
+		}
+	}
 }
