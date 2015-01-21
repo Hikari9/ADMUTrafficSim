8ffc4a82bd9b0d8e77ea4b2b921fa003f1bf5faf
Rico Tiongson <thericotiongson@gmail.com>
2015-01-21 14:24:56 +0800

    Added command receiver script.

--- Assets/Scripts/CommandReceiver.cs
+++ Assets/Scripts/CommandReceiver.cs
@@ -3,10 +3,17 @@ using System.Collections;
 
 public class CommandReceiver : MonoBehaviour {
 
+	public bool enableSpawn = false;
+
 	public string spawnNorth = "up";
 	public string spawnEast = "right";
 	public string spawnSouth = "down";
 	public string spawnWest = "left";
+
+	public bool enableKeyCommands = false;
+
+	public string stopCommand = "s";
+	public string goCommand = "g";
 
 	CarSpawner spawn = null;
 
@@ -18,16 +25,27 @@ public class CommandReceiver : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		try {
-			if (Input.GetKeyDown (spawnNorth))
-				spawn.SpawnNorth();
-			if (Input.GetKeyDown (spawnSouth))
-				spawn.SpawnSouth();
-			if (Input.GetKeyDown (spawnEast))
-				spawn.SpawnEast();
-			if (Input.GetKeyDown(spawnWest))
-				spawn.SpawnWest();
+			if (enableSpawn) {
+				if (Input.GetKeyDown (spawnNorth))
+					spawn.SpawnNorth();
+				if (Input.GetKeyDown (spawnSouth))
+					spawn.SpawnSouth();
+				if (Input.GetKeyDown (spawnEast))
+					spawn.SpawnEast();
+				if (Input.GetKeyDown(spawnWest))
+					spawn.SpawnWest();
+			}
+			if (enableKeyCommands) {
+				if (Input.GetKeyDown (this.goCommand)) {
+					GameObject.FindGameObjectWithTag("GameController").GetComponent<GoCommand>().PerformCommand ();
+				}
+				else if (Input.GetKeyDown (this.stopCommand)) {
+					GameObject.FindGameObjectWithTag("GameController").GetComponent<StopCommand>().PerformCommand ();
+				}
+			}
 		}
-		catch {
+		catch(UnityException e) {
+			Debug.Log (e);
 			Debug.Log ("Exception thrown.");
 		}
 	}
